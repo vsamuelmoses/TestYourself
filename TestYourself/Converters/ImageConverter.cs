41892993c2a017fad67852e44da460b8b3d82fb7
Samuel M Vemagiri <vsamuelmoses@gmail.com>
2014-08-18 22:34:45 +0100

    UI Enhancements

--- TestYourself/Converters/ImageConverter.cs
+++ TestYourself/Converters/ImageConverter.cs
@@ -1,26 +1,28 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows.Data;
-using TestYourself.Model;
 
 namespace TestYourself.Converters
 {
-    public class ImageConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            if (value == null) 
-                return null;
+	public class ImageConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+			if (value == null) 
+				return null;
 
-            return ("../Databases/DrivingTheory/" + value);
+			if(((string)value).StartsWith("DrivingTheory"))
+				return ("../Databases/" + value);
+			else
+				return ("../Databases/DrivingTheory/" + value);
 
-        }
+		}
 
-        public object ConvertBack(object value, Type targetType,
-            object parameter, CultureInfo culture)
-        {
-            return null;
-        }
-    }
+		public object ConvertBack(object value, Type targetType,
+			object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
 }
