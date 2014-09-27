c805dd394efb644ac07943af05bf9a3020263ac6
Samuel M Vemagiri <vsamuelmoses@gmail.com>
2014-09-27 13:36:10 +0100

    Marking set

--- TestYourself/Converters/QuestionToMarkNumberOfAnswersTextConverter.cs
+++ TestYourself/Converters/QuestionToMarkNumberOfAnswersTextConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using System.Linq;
+using System.Windows;
 using System.Windows.Data;
 using TestYourself.Model;
 
@@ -18,6 +19,25 @@ namespace TestYourself.Converters
 
 			return string.Empty;
 
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class MarkNumerOfAnswersVisibilityConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var question = (Question)value;
+			var numberOfCorrectAnswers = question.Answers.Count(answer => answer.IsCorrect);
+
+			if (numberOfCorrectAnswers > 1)
+				return Visibility.Visible;
+
+			return Visibility.Collapsed;
 		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
